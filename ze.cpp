d75073bd4f5887bb1685789731af4129bcdc0715
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-12-10 13:27:25 +0000

    Update Ze

--- ze.cpp
+++ ze.cpp
@@ -1,21 +1,180 @@
+//
+// Ze Console
+//
+// description: Console class for Windows
+// author: José Vieira Lisboa
+// url: https://github.com/joselisboa/ze
+//
+#include <iostream>
 #include <windows.h>
-#include "ze.h"
+#include <string>
+#include "c:\\htdocs\\ze\\ze.h"
 
-void Ze::show_console_cursor(bool bVisible)
+using namespace std;
+
+Ze &Ze::showCursor(bool bVisible)
 {
-    GetConsoleCursorInfo(_out, &_cci);
-    _cci.bVisible = bVisible;// cursor visibility
-    SetConsoleCursorInfo(_out, &_cci);
+    GetConsoleCursorInfo(_output_handle, &cursor_info);
+    cursor_info.bVisible = bVisible;// cursor visibility
+    SetConsoleCursorInfo(_output_handle, &cursor_info);
+    return *this;
 }
 
-void Ze::cursor_size(const int dwSize)// dwSize: 1 to 99
+Ze &Ze::cursorSize(const int dwSize)// dwSize: 1 to 99
 {
-    _cci.dwSize = dwSize;
-    _cci.bVisible = true;
-    SetConsoleCursorInfo(_out, &_cci);
+    cursor_info.dwSize = dwSize;
+    cursor_info.bVisible = true;
+    SetConsoleCursorInfo(_output_handle, &cursor_info);
+    return *this;
 }
 
-int Ze::cursor_size() const
+Ze &Ze::paintString(string text, COORD pos, WORD cor)
 {
-    return _cci.dwSize;
+    WORD aux = textColor();
+
+    SetConsoleTextAttribute(_output_handle, cor);
+    SetConsoleCursorPosition(_output_handle, pos);
+    cout << text;
+
+    textColor(aux);
+
+    return *this;
+}
+
+Ze &Ze::textSize(int x, int y)
+{
+    CONSOLE_FONT_INFOEX cfont;
+    cfont.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+    GetCurrentConsoleFontEx(_output_handle, false, &cfont);
+    cfont.dwFontSize.X = x;
+    cfont.dwFontSize.Y = y;
+    SetCurrentConsoleFontEx(_output_handle, false, &cfont);
+    return *this;
+}
+
+WORD Ze::textColor()
+{
+    refresh();
+    return screen_info.wAttributes;
+}
+
+Ze &Ze::foreground(WORD color)
+{
+    SetConsoleTextAttribute(_output_handle, color);
+    return *this;
+}
+
+Ze &Ze::background(WORD color)
+{
+    foreground(color << 4);
+    return *this;
+}
+
+Ze &Ze::screenSize(int width, int height) {
+    SMALL_RECT DisplayArea;
+    SetConsoleScreenBufferSize(_output_handle, COORD {width, height });
+    DisplayArea.Top = 0;
+    DisplayArea.Left = 0;
+    DisplayArea.Bottom = height - 1;
+    DisplayArea.Right = width - 1;
+    SetConsoleWindowInfo(_output_handle, TRUE, &DisplayArea);
+    //return refresh();
+    return *this;
+}
+
+Ze &Ze::refresh()
+{
+    GetConsoleScreenBufferInfo(_output_handle, &screen_info);
+    return *this;
+}
+
+Ze::Ze()
+{
+    _input_handle = GetStdHandle(STD_INPUT_HANDLE);
+    _output_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // perde-se controlo do input (?)
+    //SetConsoleMode(_input_handle, ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+
+    GetConsoleScreenBufferInfo(_output_handle, &screen_info);
+}
+
+char Ze::ch()
+{
+    char ch;
+
+    // ler input da consola
+    ReadConsoleInput(input(), &input_record, 1, &info);
+
+    // evento do teclado
+    if (input_record.EventType == KEY_EVENT && input_record.Event.KeyEvent.bKeyDown) {
+        ch = (char) input_record.Event.KeyEvent.wVirtualKeyCode;
+    }
+    else ch = '\0';
+
+    // limpar o buffer de input da consola
+    FlushConsoleInputBuffer(input());
+    return ch;
+}
+
+Ze &Ze::ch(char ch, COORD pos)
+{
+    cursorPosition(pos);
+    cout << ch;
+    return *this;
+}
+
+Ze &Ze::cls(WORD background)
+{
+    this->background(background);
+    system("cls");
+    return *this;
+}
+
+Ze &Ze::_input()
+{
+    // ler input da consola
+    ReadConsoleInput(_input_handle, &input_record, 1, &info);
+    cursorPosition(0, 0);
+
+    // evento do rato
+    if (input_record.EventType == MOUSE_EVENT) {
+    COORD coord;
+    coord.X = input_record.Event.MouseEvent.dwMousePosition.X;
+    coord.Y = input_record.Event.MouseEvent.dwMousePosition.Y;
+    switch (input_record.Event.MouseEvent.dwButtonState) {
+    case FROM_LEFT_1ST_BUTTON_PRESSED:
+    cout << "Mouse[" << coord.X << "," << coord.Y << "]";
+    break;
+    default:;
+    cout << "DEFAULT[" << coord.X << "," << coord.Y << "]";
+    }
+    }
+
+    // evento do teclado
+    if (input_record.EventType == KEY_EVENT && input_record.Event.KeyEvent.bKeyDown) {
+        switch (input_record.Event.KeyEvent.wVirtualKeyCode) {
+        case VK_RIGHT:
+            cout << "RIGHT";
+            break;
+        case VK_LEFT:
+            cout << "LEFT";
+            break;
+        case VK_UP:
+            cout << "UP";
+            break;
+        case VK_DOWN:
+            cout << "DOWN";
+            break;
+        default:
+            cout << (char)input_record.Event.KeyEvent.wVirtualKeyCode;
+        }
+        cout << " (" << input_record.Event.KeyEvent.wVirtualKeyCode << ")    ";
+    }
+
+    // limpar o buffer de input da consola
+    FlushConsoleInputBuffer(_input_handle);
+
+    // acorrentar ...
+    return *this;
 }
--- ze.h
+++ ze.h
@@ -1,18 +1,70 @@
+//
+// Ze Console
+//
+// description: Console class for Windows
+// author: José Vieira Lisboa
+// url: https://github.com/joselisboa/ze
+//
 #ifndef _ZE_H_
 #define _ZE_H_
-#include "windows.h"
+
+#include <windows.h>
+#include <string>
+
+using namespace std;
 
 class Ze
 {
 private:
-    HANDLE _out;
-    CONSOLE_CURSOR_INFO _cci;
+    HANDLE _input_handle;
+    HANDLE _output_handle;
 
 public:
-    void show_console_cursor(bool bVisible);
-    void cursor_size(const int dwSize = 0);
-    int cursor_size() const;
-    Ze() { _out = GetStdHandle(STD_OUTPUT_HANDLE); }
+    WORD BLACK = 0;
+    WORD BLUE_FADE = FOREGROUND_BLUE;
+    WORD GREEN_FADE = FOREGROUND_GREEN;
+    WORD CYAN_FADE = FOREGROUND_GREEN | FOREGROUND_BLUE;
+    WORD RED_FADE = FOREGROUND_RED;
+    WORD MAGENTA_FADE = FOREGROUND_RED | FOREGROUND_BLUE;
+    WORD YELLOW_FADE = FOREGROUND_RED | FOREGROUND_GREEN;
+    WORD WHITE_FADE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    WORD GRAY = FOREGROUND_INTENSITY;
+    WORD BLUE = FOREGROUND_INTENSITY | FOREGROUND_BLUE;
+    WORD GREEN = FOREGROUND_INTENSITY | FOREGROUND_GREEN;
+    WORD CYAN = FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    WORD RED = FOREGROUND_INTENSITY | FOREGROUND_RED;
+    WORD MAGENTA = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE;
+    WORD YELLOW = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN;
+    WORD WHITE = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    DWORD info;
+    INPUT_RECORD input_record;
+    CONSOLE_CURSOR_INFO cursor_info;
+    CONSOLE_SCREEN_BUFFER_INFO screen_info;
+    WORD textColor();
+    BOOL textColor(WORD color) { return SetConsoleTextAttribute(_output_handle, color); }
+    BOOL resetTextColor() { return SetConsoleTextAttribute(_output_handle, WHITE_FADE); }   
+    Ze &foreground(WORD);
+    Ze &background(WORD);
+    Ze &paintString(string, COORD, WORD);
+    Ze &showCursor(bool bVisible = true);
+    Ze &hideCursor(bool bVisible = false) { showCursor(bVisible); return *this; }
+    Ze &cursorSize(const int dwSize = 0);
+    Ze &cursorPosition(short x, short y) { cursorPosition(COORD{ x, y }); return *this; }
+    Ze &cursorPosition(COORD pos) { SetConsoleCursorPosition(output(), pos); return *this; }
+    Ze &textSize(int, int);
+    Ze &screenSize(int, int);
+    Ze &refresh();
+    Ze &ch(char ch, COORD pos = { 0, 0 });
+    Ze &cls(WORD);
+    char ch();
+
+    int cursorSize() { return cursor_info.dwSize; }
+    Ze();
+
+    //TODO remover estas funções
+    HANDLE input() { return _input_handle; };
+    HANDLE output() { return _output_handle; };
+    Ze &_input();
 };
 
 #endif
